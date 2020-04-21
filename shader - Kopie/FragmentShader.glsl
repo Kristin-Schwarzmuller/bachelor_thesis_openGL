a7be5cec07fc321686e003ec78dcbe0ad03283c7
hm-schwar46 <k.schwarzmueller@hm.edu>
2020-04-21 23:21:37 +0200

    phong added in shader

---
+++ shader - Kopie/FragmentShader.glsl
@@ -0,0 +1,154 @@
+#version 430 // change later
+
+
+
+// Structures
+// =============================================================================================================
+struct Matrices
+{
+	mat4 mvp;
+	mat4 mv;
+	mat3 normal;
+    mat4 bmvp;
+};
+
+
+struct Light
+{
+    vec3 lightPos;
+	vec4 ambient;
+	vec4 diffus;
+	vec4 specular;
+};
+
+struct Material 
+{
+	vec4 ambient;
+	vec4 diffus;
+	vec4 spekular;
+	vec4 emissiv;
+	float shininess;
+};
+
+
+struct FragmentInput
+{
+    vec3 normal;
+    vec3 surfaceNormal;
+
+	vec3 lightDir;
+	vec3 viewDir;
+
+    vec4 shadow_coordinates;
+};
+
+struct Normalized
+{
+    vec3 normal;
+    vec3 lightDir;
+    vec3 viewDir;
+};
+
+struct Textures
+{
+    sampler2DShadow shadowmap;
+};
+// =============================================================================================================
+
+
+
+
+// Subroutines
+// =============================================================================================================
+subroutine void FragmentProgram();
+// =============================================================================================================
+
+
+
+
+// Variables
+// =============================================================================================================
+layout(location = 0) in FragmentInput Input;
+
+uniform Matrices matrices;
+
+uniform Light light;
+uniform Material material;
+
+uniform Textures tex;
+
+subroutine uniform FragmentProgram fragmentprogram;
+
+layout(location = 0) out vec4 out_color;
+// =============================================================================================================
+
+
+
+
+// Methods
+// =============================================================================================================
+void main()
+{
+	fragmentprogram();
+}
+// =============================================================================================================
+
+
+
+
+// Subroutine Implementation
+// =============================================================================================================
+layout (index = 0) subroutine (FragmentProgram) void lambert()
+{
+    Normalized n;
+    n.viewDir = normalize(Input.viewDir);
+    n.lightDir = normalize(Input.lightDir);
+    n.normal = normalize(Input.normal);
+
+    float intensity = dot(n.lightDir, n.normal);
+
+
+    vec3 shadow_coordinates = Input.shadow_coordinates.xyz;
+//    shadow_coordinates.z -= (.0001f + (1.f - intensity) * .005f); // Needs to be adjusted
+
+    float shadowsample = clamp(texture(tex.shadowmap, shadow_coordinates), .35f, 1.f);
+
+
+    //out_color = vec4(1); // just white
+    //shadowsample * intensity * vec4(.7f);
+    out_color = shadowsample * intensity * vec4(.7f);
+    out_color.w = 1.f;
+}
+
+
+subroutine (FragmentProgram) void depthmap()
+{
+    out_color = vec4(gl_FragCoord.z);
+}
+
+
+layout (index = 1) subroutine (FragmentProgram) void phong()
+{
+
+    Normalized n;
+    n.viewDir = normalize(Input.viewDir);
+    n.lightDir = normalize(Input.lightDir);
+    n.normal = normalize(Input.normal);
+
+    // Ambient light
+    vec4 ambient = light.ambient * material.ambient; 
+
+    // Diffuse light
+	float d = dot(n.normal, n.lightDir);
+	vec4 diffus = d * light.diffus * material.diffus;
+
+    // Specular
+	vec4 specular = vec4(0.f, 0.f, 0.f, 1.f);
+	if (d > 0.f) {
+		vec3 r = reflect(-n.lightDir, n.normal);
+		specular = pow(max(dot(normalize (r), n.normal), 0), material.shininess)*light.specular*material.spekular;
+	}
+	
+	out_color = material.emissiv + ambient  + specular + diffus;
+}
+// =============================================================================================================
---
+++ shader - Kopie/VertexShader.glsl
@@ -0,0 +1,98 @@
+#version 410
+
+
+
+// Structures
+// =============================================================================================================
+struct Matrices
+{
+	mat4 mvp;
+	mat4 mv;
+	mat3 normal;
+    mat4 bmvp;
+};
+
+
+struct Light
+{
+    vec3 lightPos;
+	vec4 ambient;
+	vec4 diffus;
+	vec4 specular;
+};
+
+
+struct VertexOutput
+{
+    vec3 normal;
+    vec3 surfaceNormal;
+
+	vec3 lightDir;
+	vec3 viewDir;
+
+    vec4 shadow_coordinates;
+};
+// =============================================================================================================
+
+
+
+
+// Subroutines
+// =============================================================================================================
+subroutine void VertexProgram();
+// =============================================================================================================
+
+
+
+
+// Variables
+// =============================================================================================================
+layout(location = 0) in vec4 vertex;
+layout(location = 1) in vec3 normal;
+
+uniform Matrices matrices;
+
+uniform Light light;
+
+subroutine uniform VertexProgram vertexprogram;
+
+layout(location = 0) out VertexOutput Output;
+// =============================================================================================================
+
+
+
+
+// Methods
+// =============================================================================================================
+void main()
+{
+	vertexprogram();
+}
+// =============================================================================================================
+
+
+
+
+// Subroutine Implementation
+// =============================================================================================================
+subroutine (VertexProgram) void verts_and_normals()
+{
+    gl_Position = matrices.mvp * vertex;
+
+    Output.normal = matrices.normal * normal;
+    Output.surfaceNormal = normal;
+
+    vec4 h = matrices.mv * vertex;
+    vec3 mvPos = h.xyz / h.w;
+
+    Output.lightDir = light.lightPos - mvPos;
+    Output.viewDir = -mvPos;
+
+    Output.shadow_coordinates = matrices.bmvp * vertex;
+}
+
+subroutine (VertexProgram) void simple_placement()
+{
+    gl_Position = matrices.mvp * vertex;
+}
+// =============================================================================================================
